3a3a46f8376ba207ce76c1f8282803a3eb623dc3
aungphyo122000dia-ai <aungphyo122000dia@gmail.com>
2026-05-09 23:58:05 +0630

    Update server.js

--- server.js
+++ server.js
@@ -78,3 +78,6 @@ app.post("/voice", async (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+app.get("/", (req, res) => {
+  res.send("AI Server is Running ✅");
+});
